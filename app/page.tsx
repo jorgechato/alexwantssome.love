a9ebb5ffd96018a61a7b19cd2087450aac3e42c0
jorgechato <hack@jorgechato.com>
2023-05-24 00:20:04 +0900

    FIX widths on sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between
     bg-gradient-to-b from-blue-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
-      <div className="bg-blue-500">
+      <div className="bg-blue-500 w-full">
         <img src="/sticker.png" className="brightness-125 px-20 pt-10" alt="" />
       </div>
-      <div className="bg-emerald-500">
-      <img src="/Subject.png" className="pt-10 pb-3 px-2" alt="" />
+      <div className="bg-emerald-500 w-full">
+        <img src="/Subject.png" className="pt-10 pb-3 px-2" alt="" />
       </div>
-      <div className="w-full min-h-40 h-80 bg-cover" style={{backgroundImage: "url(/IMG_2572.jpg)"}}>
+      <div className="w-full min-h-40 h-80 bg-cover" style={{ backgroundImage: "url(/IMG_2572.jpg)" }}>
       </div>
     </main>
   )
